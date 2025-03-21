3ff64f325a80e865d60440c204b1d531c838f405
mustakin04 <mustakinhasan37@gmail.com>
2025-03-22 01:51:28 +0600

    Membership part complete

--- my-project/src/Components/Membership/Membership.jsx
+++ my-project/src/Components/Membership/Membership.jsx
@@ -1,16 +1,161 @@
-import React from 'react'
-import Container from '../Container/Container'
+import React from "react";
+import Container from "../Container/Container";
 
 const Membership = () => {
   return (
-    <div className='bg-[#000000] py-[128px]'>
-        <Container>
-            <h1 className='font-primary font-semibold text-whites text-[64px] text-center'>
-                Membership</h1>
-            <p>If a $500 ball isn't</p>
-        </Container>
+    <div className="bg-[#000000] py-[128px]">
+      <Container>
+        <h1 className="font-primary font-semibold text-whites text-[64px] text-center">
+          Membership
+        </h1>
+        <p className="font-primary font-medium text-whites text-[24px] w-[718px] m-auto text-center mt-[64px]">
+          If a $500 ball isn't enough,you can improve our margins further by
+          kindly providing us with recurring revenue.
+        </p>
+        <div className="mt-[65px] flex justify-between">
+           {/* BASIC part start */}
+        <div className="w-[421px] font-primary text-whites border-2 border-[#0D3317] rounded-[8px]">
+            <h2
+              className="font-normal text-[32px] text-center border-b-2 border-[#0D3317] pt-[48px]
+             pb-[24px] border-opacity-25 mx-[20px]"
+            >
+              BASIC
+            </h2>
+            <h3
+              className="font-normal text-[32px] text-center border-b-2 border-[#0D3317] pt-[48px]
+             pb-[24px] border-opacity-25 mx-[20px]"
+            >
+              FREE
+            </h3>
+            <h4
+              className="font-normal text-[18px] text-center border-b-2 border-[#0D3317] pt-[48px]
+             pb-[24px] border-opacity-25 mx-[20px]"
+            >
+              "Evil" mode only
+            </h4>
+            <p
+              className="font-normal text-[18px] text-center border-b-2 border-[#0D3317] pt-[48px]
+             pb-[24px] border-opacity-25 mx-[20px]"
+            >
+              No saved data
+            </p>
+            <div className="pt-[48px]
+             pb-[24px]">
+              <div
+                className="flex w-[276px] m-auto gap-3.5 py-[19px] px-[36px] rounded-[999px] 
+bg-[rgb(28,30,83)] bg-gradient-to-br from-[rgba(28,30,83,0)] to-[rgba(13,51,23,1)] 
+shadow-[0px_0px_40px_5px_rgba(40,153,66,0.75)] 
+transition-all duration-300 ease-in-out 
+hover:bg-[rgb(13,51,23)] hover:bg-gradient-to-br hover:from-[rgba(13,51,23,0)] hover:to-[rgba(33,133,57,1)]"
+              >
+                <a
+                  href=""
+                  className="font-primary font-semibold text-[18px] text-whites"
+                >
+                  Get Forecaster
+                </a>
+                <div className="w-[24px] h-[24px] bg-whites rounded-full"></div>
+              </div>
+            </div>
+          </div>
+        {/* BASIC part end */}
+        {/* PRO part start*/}
+        <div className="w-[421px] font-primary text-whites border-2 border-[#0D3317] rounded-[8px]">
+            <h2
+              className="font-normal text-[32px] text-center border-b-2 border-[#0D3317] pt-[48px]
+             pb-[24px] border-opacity-25 mx-[20px]"
+            >
+              BASIC
+            </h2>
+            <h3
+              className="font-normal text-[32px] text-center border-b-2 border-[#0D3317] pt-[48px]
+             pb-[24px] border-opacity-25 mx-[20px]"
+            >
+              FREE
+            </h3>
+            <h4
+              className="font-normal text-[18px] text-center border-b-2 border-[#0D3317] pt-[48px]
+             pb-[24px] border-opacity-25 mx-[20px]"
+            >
+              "Evil" mode only
+            </h4>
+            <p
+              className="font-normal text-[18px] text-center border-b-2 border-[#0D3317] pt-[48px]
+             pb-[24px] border-opacity-25 mx-[20px]"
+            >
+              No saved data
+            </p>
+            <div className="pt-[48px]
+             pb-[24px]">
+              <div
+                className="flex w-[276px] m-auto gap-3.5 py-[19px] px-[36px] rounded-[999px] 
+bg-[rgb(28,30,83)] bg-gradient-to-br from-[rgba(28,30,83,0)] to-[rgba(13,51,23,1)] 
+shadow-[0px_0px_40px_5px_rgba(40,153,66,0.75)] 
+transition-all duration-300 ease-in-out 
+hover:bg-[rgb(13,51,23)] hover:bg-gradient-to-br hover:from-[rgba(13,51,23,0)] hover:to-[rgba(33,133,57,1)]"
+              >
+                <a
+                  href=""
+                  className="font-primary font-semibold text-[18px] text-whites"
+                >
+                  Get Forecaster
+                </a>
+                <div className="w-[24px] h-[24px] bg-whites rounded-full"></div>
+              </div>
+            </div>
+          </div>
+        {/* Pro part end */}
+        {/* PREMIUM part start */}
+        <div className="w-[421px] font-primary text-whites border-2 border-[#0D3317] rounded-[8px]">
+            <h2
+              className="font-normal text-[32px] text-center border-b-2 border-[#0D3317] pt-[48px]
+             pb-[24px] border-opacity-25 mx-[20px]"
+            >
+              BASIC
+            </h2>
+            <h3
+              className="font-normal text-[32px] text-center border-b-2 border-[#0D3317] pt-[48px]
+             pb-[24px] border-opacity-25 mx-[20px]"
+            >
+              FREE
+            </h3>
+            <h4
+              className="font-normal text-[18px] text-center border-b-2 border-[#0D3317] pt-[48px]
+             pb-[24px] border-opacity-25 mx-[20px]"
+            >
+              "Evil" mode only
+            </h4>
+            <p
+              className="font-normal text-[18px] text-center border-b-2 border-[#0D3317] pt-[48px]
+             pb-[24px] border-opacity-25 mx-[20px]"
+            >
+              No saved data
+            </p>
+            <div className="pt-[48px]
+             pb-[24px]">
+              <div
+                className="flex w-[276px] m-auto gap-3.5 py-[19px] px-[36px] rounded-[999px] 
+bg-[rgb(28,30,83)] bg-gradient-to-br from-[rgba(28,30,83,0)] to-[rgba(13,51,23,1)] 
+shadow-[0px_0px_40px_5px_rgba(40,153,66,0.75)] 
+transition-all duration-300 ease-in-out 
+hover:bg-[rgb(13,51,23)] hover:bg-gradient-to-br hover:from-[rgba(13,51,23,0)] hover:to-[rgba(33,133,57,1)]"
+              >
+                <a
+                  href=""
+                  className="font-primary font-semibold text-[18px] text-whites"
+                >
+                  Get Forecaster
+                </a>
+                <div className="w-[24px] h-[24px] bg-whites rounded-full"></div>
+              </div>
+            </div>
+          </div>
+        {/* PREMIUM part end */}
+        </div>
+         <h6 className="font-primary font-normal text-whites text-[16px] text-center mt-[64px]">*Plans and prices are subject to change based on the whims of our CRO</h6>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Membership
+export default Membership;
